a5ef50d349267d6d44f4c009b2a9db57aba9e161
Andrey Kartashov <andrey.kartashov@gmail.com>
2018-11-20 10:45:49 -0500

    updates

--- Main.hs
+++ Main.hs
@@ -13,7 +13,6 @@ import qualified Data.Conduit.List as CL
 import           Data.List
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import           Data.Monoid
 import           GHC.IO.Handle (hSetBinaryMode, hSetBuffering, BufferMode(..))
 import           System.Environment (getArgs)
 import           System.IO (stdin, stdout)
@@ -23,7 +22,7 @@ main :: IO ()
 main = do
   cliArgs <- getArgs
 
-  if ((not.null) cliArgs)
+  if (not.null) cliArgs
   then putStrLn "Reads from STDIN, writest to STDOUT, output is same as from 'sort | uniq -c | sort -n'"
   else runMain
 
@@ -36,22 +35,15 @@ runMain = do
   hSetBinaryMode stdout True
   hSetBuffering stdout (BlockBuffering Nothing)
 
-  l2c <- stdinLines $$ countLines
-  let orderedL2c = sortOn (snd) $ Map.toList l2c
+  l2c <- runConduit $  CB.sourceHandle stdin .| CB.lines .| countLines
+  let orderedL2c = sortOn snd $ Map.toList l2c
 
 
-  forM_ orderedL2c $ \(l,c) -> do
-    BSB.hPutBuilder stdout $ (BSB.wordDec c) <> (BSB.byteString "\t") <> (BSB.byteString l) <> (BSB.byteString "\n")
+  forM_ orderedL2c $ \(l,c) ->
+    BSB.hPutBuilder stdout $ BSB.wordDec c <> BSB.byteString "\t" <> BSB.byteString l <> BSB.byteString "\n"
 
 
 countLines :: (MonadIO m)
-           => Consumer ByteString m (Map ByteString Word)
+           => ConduitT ByteString Void m (Map ByteString Word)
 countLines = CL.fold addLine Map.empty
   where addLine !l2c !l = Map.insertWith (+) l 1 l2c
-
-
-stdinLines :: (MonadIO m)
-           => Producer m ByteString
-stdinLines = do
-  CB.sourceHandle stdin =$= CB.lines
-
